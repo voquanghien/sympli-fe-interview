36dd34799a2262cc4c7592706eaad3e1d1a4985b
Hien Vo <vqhien94@gmail.com>
2025-01-08 16:24:37 +0700

    add format script (prettier)

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
-import './index.css'
-import App from './App.tsx'
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
